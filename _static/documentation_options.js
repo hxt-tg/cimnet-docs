75a70d330803a6f8685e97bae9da711e28fe9d74
hxt-tg <782920705@qq.com>
2021-02-05 15:21:45 +0800

    docs update v0.1.4

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,12 +1,9 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.3',
+    VERSION: '0.1.4',
     LANGUAGE: 'zh_CN',
     COLLAPSE_INDEX: false,
-    BUILDER: 'html',
     FILE_SUFFIX: '.html',
-    LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
-    SOURCELINK_SUFFIX: '.txt',
-    NAVIGATION_WITH_KEYS: false
+    SOURCELINK_SUFFIX: '.txt'
 };
